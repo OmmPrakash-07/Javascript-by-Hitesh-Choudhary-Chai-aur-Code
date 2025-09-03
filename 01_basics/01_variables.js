388db841a7d275648f522faa9eca861b11057870
Omm Prakash Parida <ommprakashparida114@gmail.com>
2025-09-04 00:30:42 +0530

    update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 520698165101                                      // Can't Change value
+let accountEmail = "ommprakashparida114@gmail.com"
+var accountPassword = "123456"
+accountCity = "Cuttack"
+let accountState;
+
+// accountId = 2
+accountEmail = "geniusomm07@gmail.com"
+accountPassword = "456321"
+accountCity = "Bbsr"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table ([accountId, accountEmail, accountPassword, accountCity, accountState])
